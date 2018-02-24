ead0908a2a28a1ca8e58cb3e316f14d23db56b45
Sergey Zylevich <sergey496@yandex.ru>
2018-02-24 19:41:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+     if (array.length==0){return 0;}
+   if (array.length==1){return 1;}
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+
+
+
+array.sort(compareNumeric);
+var sum=[];
+var j=0;
+var count=1;
+for (var i=1; i<array.length;i++){
+	if ((array[i]-array[i-1])==1){
+		sum[j]=count;
+		count++;
+		j++;
+	}
+	else {count =1;}
+	
+}
+sum.sort(compareNumeric);
+ 
+return (sum[sum.length-1]+1); 
 }
